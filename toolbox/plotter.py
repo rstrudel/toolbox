0633b1b7e4d01c5a2e9e8c37d71f1ea6de8899a6
rstrudel <rstrudel@gmail.com>
2020-09-29 13:27:18 +0200

    plotter: add line class and image

--- toolbox/plotter.py
+++ toolbox/plotter.py
@@ -4,33 +4,87 @@ import numpy as np
 from toolbox import colors
 
 
+class Line:
+    def __init__(self, ax, label):
+        self.ax = ax
+        self.line = None
+        self.data = {"x": [], "y": []}
+        self.label = label
+
+    def reset(self):
+        self.data = {"x": [], "y": []}
+
+    def min(self):
+        return (min(self.data["x"]), min(self.data["y"]))
+
+    def max(self):
+        return (max(self.data["x"]), max(self.data["y"]))
+
+    def update(self, value):
+        ax = self.ax
+        self.data["y"] = self.data["y"] + [value]
+        self.data["x"] = np.arange(len(self.data["y"]))
+        if self.line is None:
+            self.line = ax.plot(
+                self.data["x"],
+                self.data["y"],
+                "r-",
+                color=colors.pop(),
+                label=self.label,
+            )[0]
+            ax.legend()
+        else:
+            self.line.set_data(self.data["x"], self.data["y"])
+
+
 class Plotter:
-    def __init__(self):
-        self.fig, self.axs = plt.subplots(1, 1)
-        self.data = []
-        self.lines = []
+    def __init__(self, rows, columns):
+        self.fig, self.axs = plt.subplots(rows, columns)
+        if rows == 1 and columns == 1:
+            self.axs = np.array(self.axs).reshape(1, 1)
+        elif columns == 1:
+            self.axs = self.axs.reshape(-1, 1)
+        elif rows == 1:
+            self.axs = self.axs.reshape(1, -1)
+
+        self.lines = {}
+        self.ims = {}
         self.lim_factor = 1.1
 
     def reset(self):
-        self.data = []
+        self.lines = {}
+        self.ims = {}
 
-    def plot_lines(self, v):
-        self.data.append(v)
-        y = np.array(self.data)
-        x = np.arange(len(y))
-        if not self.lines:
-            self.lines = []
-            for i, column in enumerate(y.T):
-                line = self.axs.plot(x, column, "r-", color=colors.pop(), label=f"{i}")[
-                    0
-                ]
-                self.lines.append(line)
-            plt.legend()
+    def plot_line(self, value, label, ax_i=0, ax_j=0):
+        ax = self.axs[ax_i, ax_j]
+        ax_key = f"{ax_i}{ax_j}"
+        if ax_key not in self.lines:
+            self.lines[ax_key] = {}
+        if label not in self.lines[ax_key]:
+            line = Line(ax, label)
+            self.lines[ax_key][label] = line
         else:
-            for column, line in zip(y.T, self.lines):
-                line.set_data(x, column)
+            line = self.lines[ax_key][label]
+        line.update(value)
+
+        ax_lines = self.lines[ax_key].values()
+        vmin = np.vstack([line.min() for line in ax_lines]).min(axis=0)
+        vmax = np.vstack([line.max() for line in ax_lines]).max(axis=0)
+        idx_scale = np.where(vmax <= vmin)
+        vmax[idx_scale] = 1.1 * vmin[idx_scale] + 1e-5
 
         lf = self.lim_factor
-        self.axs.set_xlim(lf * x.min(), lf * x.max())
-        self.axs.set_ylim(lf * y.min(), lf * y.max())
-        plt.pause(1e-5)
+        ax.set_xlim(lf * vmin[0], lf * vmax[0])
+        ax.set_ylim(lf * vmin[1], lf * vmax[1])
+
+    def plot_im(self, A, label, ax_i=0, ax_j=0, **imshow_kwargs):
+        ax = self.axs[ax_i, ax_j]
+        ax_key = f"{ax_i}{ax_j}"
+        if ax_key not in self.ims:
+            self.ims[ax_key] = ax.imshow(A, **imshow_kwargs)
+        else:
+            im = self.ims[ax_key]
+            im.set_data(A)
+
+    def show(self, timeout=1e-5):
+        plt.pause(timeout)
